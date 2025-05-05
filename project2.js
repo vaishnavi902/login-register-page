a384756ebd6e6d81fc82ed82c24585aa83540d8b
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2025-05-05 16:10:46 +0530

    Add files via upload

---
+++ project2.js
@@ -0,0 +1,17 @@
+/*const form=document.querySelector('.form');
+const loginlink=document.querySelector('.box-login');
+const registerlink = document.querySelector('.register-link');
+const btnpopup=document.querySelector('.btnLogin-popup')
+const iconclose=document.querySelector('.icon-close');
+register-loginlink.addEventListener('click',()=>{
+    form.classList.add('active');
+});
+loginLink.addEventListener('click',()=>{
+    form.classList.remove('active');
+});
+btnpopup.addEventListener('click',()=>{
+    form.classList.add('active-popup');
+});
+iconclose.addEventListener('click',()=>{
+    form.classList.remove('active-popup');
+});*/
